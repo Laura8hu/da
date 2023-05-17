028a341e27ad7570883a2d610c53d314714f5156
lingyun <lingyun.hu1992@gmail.com>
2023-05-17 12:11:55 +0800

    4.0

--- login.py
+++ login.py
@@ -1,13 +1,5 @@
 num1=10; #clientA
 num4=40; #clientB
 num2=20; #clientB
-
-<<<<<<< HEAD
-num2=20; #clientA
-
-num3=30; #clientA
-
 numb5=50 #clientB
-=======
 num3=30; #clientA
->>>>>>> 940179159a5d92ab42b8fe1e82615342828f7084
